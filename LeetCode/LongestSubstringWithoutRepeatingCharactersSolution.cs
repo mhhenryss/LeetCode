52328481a0a7bd0e79c455db34c459c84e45fc3a
mhhenryss <mhhenryss@gmail.com>
2021-04-26 00:55:13 +0800

    solve LengthOfLongestSubstring

---
+++ LeetCode/LongestSubstringWithoutRepeatingCharactersSolution.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class LongestSubstringWithoutRepeatingCharactersSolution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var charArray = s.ToCharArray();
+            var result = 0;
+            var tResult = 0;
+            var head = 0;
+            var dic = new Dictionary<char, int>();
+
+            for (var i = 0; i < charArray.Length; i++)
+            {
+                if (dic.ContainsKey(charArray[i]) && dic[charArray[i]] >= head)
+                {
+                    head = dic[charArray[i]] + 1;
+                    tResult = i - dic[charArray[i]];
+                }
+                else
+                {
+                    tResult++;
+                }
+                dic[charArray[i]] = i;
+                result = result > tResult ? result : tResult;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTest/TempTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Tests
+{
+    [TestFixture()]
+    public class TempTest
+    {
+        [Test()]
+        public void Test1()
+        {
+            var lengthOfLongestSubstring = new LongestSubstringWithoutRepeatingCharactersSolution().LengthOfLongestSubstring("abba");
+        }
+    }
+}

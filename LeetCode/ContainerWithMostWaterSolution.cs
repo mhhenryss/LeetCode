5e52da27e5e83c80578b8ff96384dddb4f36e50c
mhhenryss <mhhenryss@gmail.com>
2021-04-27 00:33:09 +0800

    solve ContainerWithMostWater

---
+++ LeetCode/ContainerWithMostWaterSolution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace LeetCode
+{
+    public class ContainerWithMostWaterSolution
+    {
+        public int MaxArea(int[] height)
+        {
+            var result = 0;
+            var l = 0;
+            var r = height.Length - 1;
+
+            while (r > l)
+            {
+                var curArea = (r - l) * Math.Min(height[l], height[r]);
+                if (curArea > result)
+                {
+                    result = curArea;
+                }
+
+                if (height[l] >= height[r])
+                {
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
